74fe50ce139911d54328c847dcdc6bc25e22c05c
Cara Kane <cara.kane@gmail.com>
2018-01-17 16:35:05 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,19 @@
 function paragraphSelector() {
   return($('p'))
 }
+
+function lastImageSelector() {
+  return($('img')[-1])
+}
+
+function ninjaBabySelector() {
+
+}
+
+function divSelector() {
+
+}
+
+function lastImageSelector() {
+
+}
